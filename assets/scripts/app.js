c2399205f8aa9bee76125dd5b9f0c9391282d9c3
Yathaartha <maharjanyathaartha017@gmail.com>
2021-02-28 19:42:57 +0545

    adding shopping cart class

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -12,6 +12,34 @@ class Product {
   }
 }
 
+class ShoppingCart {
+  items = [];
+
+  render() {
+    const cartEl = document.createElement("section");
+    cartEl.innerHTML = `
+      <h2>Total: \$${0}</h2>
+      <button>Order Now</button>
+    `;
+    cartEl.className = "cart";
+    return cartEl;
+  }
+}
+
+class Shop {
+  render() {
+    const renderHook = document.getElementById("app");
+
+    const cart = new ShoppingCart();
+    const cartEl = cart.render();
+    const productList = new ProductList();
+    const prodListEl = productList.render();
+
+    renderHook.append(cartEl);
+    renderHook.append(prodListEl);
+  }
+}
+
 class ProductItem {
   constructor(product) {
     this.product = product;
@@ -61,7 +89,6 @@ class ProductList {
   constructor() {}
 
   render() {
-    const renderHook = document.getElementById("app");
     const prodList = document.createElement("ul");
     prodList.className = "product-list";
     for (const prod of this.products) {
@@ -69,9 +96,9 @@ class ProductList {
       const prodEl = productItem.render();
       prodList.append(prodEl);
     }
-    renderHook.append(prodList);
+    return prodList;
   }
 }
 
-const productList = new ProductList();
-productList.render();
+const shop = new Shop();
+shop.render();
