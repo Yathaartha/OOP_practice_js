85c850d78eca88080109a1abb9623634c31b2e05
Yathaartha <maharjanyathaartha017@gmail.com>
2021-02-28 19:59:59 +0545

    static methods and properties

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -15,6 +15,11 @@ class Product {
 class ShoppingCart {
   items = [];
 
+  addProduct(product) {
+    this.items.push(product);
+    this.totalOutput.innerHTML = `<h2>Total: \$${1}</h2>`;
+  }
+
   render() {
     const cartEl = document.createElement("section");
     cartEl.innerHTML = `
@@ -22,21 +27,8 @@ class ShoppingCart {
       <button>Order Now</button>
     `;
     cartEl.className = "cart";
+    this.totalOutput = cartEl.querySelector("h2");
     return cartEl;
-  }
-}
-
-class Shop {
-  render() {
-    const renderHook = document.getElementById("app");
-
-    const cart = new ShoppingCart();
-    const cartEl = cart.render();
-    const productList = new ProductList();
-    const prodListEl = productList.render();
-
-    renderHook.append(cartEl);
-    renderHook.append(prodListEl);
   }
 }
 
@@ -46,8 +38,7 @@ class ProductItem {
   }
 
   addToCart() {
-    console.log("Adding product to cart...");
-    console.log(this.product);
+    App.addProductTOCart(this.product);
   }
 
   render() {
@@ -99,6 +90,32 @@ class ProductList {
     return prodList;
   }
 }
+class Shop {
+  render() {
+    const renderHook = document.getElementById("app");
 
-const shop = new Shop();
-shop.render();
+    this.cart = new ShoppingCart();
+    const cartEl = this.cart.render();
+    const productList = new ProductList();
+    const prodListEl = productList.render();
+
+    renderHook.append(cartEl);
+    renderHook.append(prodListEl);
+  }
+}
+
+class App {
+  static cart;
+
+  static init() {
+    const shop = new Shop();
+    shop.render();
+    this.cart = shop.cart;
+  }
+
+  static addProductTOCart(product) {
+    this.cart.addProduct(product);
+  }
+}
+
+App.init();
