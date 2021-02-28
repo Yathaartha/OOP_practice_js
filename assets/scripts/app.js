34ad0d637ad658f73518cb464621502aa9bdd3df
Yathaartha <maharjanyathaartha017@gmail.com>
2021-02-28 19:28:33 +0545

    binding class methods

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -12,6 +12,36 @@ class Product {
   }
 }
 
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
+
+  addToCart() {
+    console.log("Adding product to cart...");
+    console.log(this.product);
+  }
+
+  render() {
+    const prodEl = document.createElement("li");
+    prodEl.className = "product-item";
+    prodEl.innerHTML = `
+        <div>
+          <img src="${this.product.imageUrl}" alt="${this.product.title}">
+          <div class = "product-item__content">
+            <h2>${this.product.title}</h2>
+            <h3>${this.product.price}</h3>
+            <p>${this.product.description}</p>
+            <button>Add to Cart</button>
+          </div>
+        </div>
+      `;
+    const addCartButton = prodEl.querySelector("button");
+    addCartButton.addEventListener("click", this.addToCart.bind(this));
+    return prodEl;
+  }
+}
+
 class ProductList {
   products = [
     new Product(
@@ -35,19 +65,8 @@ class ProductList {
     const prodList = document.createElement("ul");
     prodList.className = "product-list";
     for (const prod of this.products) {
-      const prodEl = document.createElement("li");
-      prodEl.className = "product-item";
-      prodEl.innerHTML = `
-        <div>
-          <img src="${prod.imageUrl}" alt="${prod.title}">
-          <div class = "product-item__content">
-            <h2>${prod.title}</h2>
-            <h3>${prod.price}</h3>
-            <p>${prod.description}</p>
-            <button>Add to Cart</button>
-          </div>
-        </div>
-      `;
+      const productItem = new ProductItem(prod);
+      const prodEl = productItem.render();
       prodList.append(prodEl);
     }
     renderHook.append(prodList);
